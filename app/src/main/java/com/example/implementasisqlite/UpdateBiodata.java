6c9f1b9a3caf350ff0428a8ed1c14b51960d7a97
chaplin100 <81203366+chaplin100@users.noreply.github.com>
2021-10-22 20:36:29 +0700

    new

--- app/src/main/java/com/example/implementasisqlite/UpdateBiodata.java
+++ app/src/main/java/com/example/implementasisqlite/UpdateBiodata.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,11 +15,12 @@ public class UpdateBiodata extends AppCompatActivity {
     protected Cursor cursor;
     DataHelper dbHelper;
     Button ton1, ton2;
-    EditText text1,text2,text3,text4,text5;
+    EditText text1, text2, text3, text4, text5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_biodata);
+
         dbHelper = new DataHelper(this);
         text1 = (EditText) findViewById(R.id.editText1);
         text2 = (EditText) findViewById(R.id.editText2);
@@ -26,11 +28,10 @@ public class UpdateBiodata extends AppCompatActivity {
         text4 = (EditText) findViewById(R.id.editText4);
         text5 = (EditText) findViewById(R.id.editText5);
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        cursor = db.rawQuery("SELECT * FROM biodata WHERE nama = '"+
-                getIntent().getStringExtra("nama")+"'",null);
+        cursor = db.rawQuery("SELECT * FROM biodata WHERE nama ='" +
+                getIntent().getStringExtra("nama") + "'", null);
         cursor.moveToFirst();
-        if(cursor.getCount()>0)
-        {
+        if (cursor.getCount() > 0){
             cursor.moveToPosition(0);
             text1.setText(cursor.getString(0).toString());
             text2.setText(cursor.getString(1).toString());
@@ -40,21 +41,30 @@ public class UpdateBiodata extends AppCompatActivity {
         }
         ton1 = (Button) findViewById(R.id.button1);
         ton2 = (Button) findViewById(R.id.button2);
-
-        ton1.setOnClickListener((arg0) -> {
-            SQLiteDatabase dbs = dbHelper.getWritableDatabase();
-            dbs.execSQL("update biodata set nama='"+
-                    text2.getText().toString() +"', tgl='"+
-                    text3.getText().toString() +"', jk='"+
-                    text4.getText().toString() +"', alamat='"+
-                    text5.getText().toString() +"', where no='"+
-                    text1.getText().toString() +"''");
-            Toast.makeText(getApplicationContext(), "Berhasil", Toast.LENGTH_LONG).show();
-            MainActivity.ma.RefreshList();
-            finish();
+        //daftarkan even onCliick pada btnSimpan
+        ton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.execSQL("update biodata set nama='"+
+                        text2.getText().toString() +"', tgl='" +
+                        text3.getText().toString()+"', jk='"+
+                        text4.getText().toString() +"', alamat='" +
+                        text5.getText().toString() + "' where no='" +
+                        text1.getText().toString()+"'");
+                Toast.makeText(getApplicationContext(), "Berhasil", Toast.LENGTH_LONG).show();
+                MainActivity.ma.RefreshList();
+                finish();
+            }
         });
-        ton2.setOnClickListener((arg0) -> {
-            finish();
+        ton2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                finish();
+            }
         });
     }
 }
